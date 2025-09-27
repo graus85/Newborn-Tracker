5d071efdccf3ead3681810705a5ad9eaf466c1b7
graus85 <graus85@gmail.com>
2025-09-27 16:15:21 +0200

    Create AuthProvider.tsx

---
+++ packages/web/src/providers/AuthProvider.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  // Implementa qui la logica reale. 
+  return <>{children}</>;
+};
+
+export default AuthProvider;
