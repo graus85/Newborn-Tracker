806974d4ae15b9c92525ab2f507e127607e6bd8c
graus85 <graus85@gmail.com>
2025-09-27 19:22:42 +0200

    Update Header.tsx

--- packages/web/src/components/Header.tsx
+++ packages/web/src/components/Header.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-const Header: React.FC = () => {
+export const Header: React.FC = () => {
   return (
     <header>
       <h1>Baby Tracker</h1>
     </header>
   );
 };
-
-export default Header;
