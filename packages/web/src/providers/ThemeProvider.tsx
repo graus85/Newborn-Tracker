16bc555d2e9dfb148dd162a7e1fdfa6395a72d04
graus85 <graus85@gmail.com>
2025-09-27 16:56:20 +0200

    Update ThemeProvider.tsx

--- packages/web/src/providers/ThemeProvider.tsx
+++ packages/web/src/providers/ThemeProvider.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 
 export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  // Espandi qui logica per gestire il tema/chiarezza/scuro/ecc.
-  // Base: restituisce solo i figli.
   return <>{children}</>;
 };
 
