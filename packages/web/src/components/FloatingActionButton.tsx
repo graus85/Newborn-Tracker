dbdb9a981ea3c7953ee5e36a94927bdfa6386458
graus85 <graus85@gmail.com>
2025-09-27 20:01:30 +0200

    Update FloatingActionButton.tsx

--- packages/web/src/components/FloatingActionButton.tsx
+++ packages/web/src/components/FloatingActionButton.tsx
@@ -1,26 +1,23 @@
 import React from "react";
 
-const FloatingActionButton: React.FC = () => {
-  return (
-    <button
-      style={{
-        position: "fixed",
-        bottom: 24,
-        right: 24,
-        width: 56,
-        height: 56,
-        borderRadius: "50%",
-        background: "#218085",
-        color: "#fff",
-        fontSize: "2rem",
-        border: "none",
-        boxShadow: "0 4px 16px #0002",
-        cursor: "pointer"
-      }}
-    >
-      +
-    </button>
-  );
-};
-
-export default FloatingActionButton;
+export const FloatingActionButton: React.FC<{ onClick?: () => void }> = ({ onClick }) => (
+  <button
+    onClick={onClick}
+    style={{
+      position: "fixed",
+      bottom: 24,
+      right: 24,
+      width: 56,
+      height: 56,
+      borderRadius: "50%",
+      background: "#218085",
+      color: "#fff",
+      fontSize: "2rem",
+      border: "none",
+      boxShadow: "0 4px 16px #0002",
+      cursor: "pointer",
+    }}
+  >
+    +
+  </button>
+);
