db7e560e1a627fc0acd81d58699aea8efbbb3d8d
graus85 <graus85@gmail.com>
2025-09-27 20:03:08 +0200

    Update BottomNavigation.tsx

--- packages/web/src/components/BottomNavigation.tsx
+++ packages/web/src/components/BottomNavigation.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-export const BottomNavigation: React.FC = () => {
-  return (
-    <nav className="bottom-navigation">
-      <button>Log</button>
-      <button>Summary</button>
-      <button>More</button>
-    </nav>
-  );
-};
+export const BottomNavigation: React.FC = () => (
+  <nav className="bottom-navigation">
+    <button>Log</button>
+    <button>Summary</button>
+    <button>More</button>
+  </nav>
+);
