9dc6f7bf3b94c0d2995fa973173841fd8e5b4cf8
graus85 <graus85@gmail.com>
2025-09-27 16:53:45 +0200

    Create Layout.tsx

---
+++ packages/web/src/providers/Layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+export const Layout: React.FC = () => {
+  return (
+    <div className="layout">
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
