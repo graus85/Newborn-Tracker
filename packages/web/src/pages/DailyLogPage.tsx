2e192ab35c4b9bcab17db0b87fecf5f7cc3f1d1d
graus85 <graus85@gmail.com>
2025-09-27 17:00:56 +0200

    Create DailyLogPage.tsx

---
+++ packages/web/src/pages/DailyLogPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DailyLogPage: React.FC = () => {
+  return <div>Daily Log Page (eventi giornalieri, pasti, pannolini...)</div>;
+};
+
+export default DailyLogPage;
