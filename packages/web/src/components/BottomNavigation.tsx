ab9a4579dd530a43c86c9aa60ba64c9f5bc6000f
graus85 <graus85@gmail.com>
2025-09-27 19:59:16 +0200

    Update BottomNavigation.tsx

--- packages/web/src/components/BottomNavigation.tsx
+++ packages/web/src/components/BottomNavigation.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 
-const BottomNavigation: React.FC = () => {
+export const BottomNavigation: React.FC = () => {
   return (
     <nav className="bottom-navigation">
-      {/* Qui puoi aggiungere i link alle pagine principali */}
       <button>Log</button>
       <button>Summary</button>
       <button>More</button>
     </nav>
   );
 };
-
-export default BottomNavigation;
